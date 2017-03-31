32d50de45938a17e5724492327a2332e78027145
Sabina Serra <sabse455@student.liu.se>
2017-03-31 11:57:03 +0200

    a little updates

--- KBD_ENC.vhd
+++ KBD_ENC.vhd
@@ -17,9 +17,8 @@ entity KBD_ENC is
 	rst		        		: in std_logic;			-- reset signal
         PS2KeyboardCLK	                        : in std_logic; 		-- USB keyboard PS2 clock
         PS2KeyboardData			        : in std_logic;			-- USB keyboard PS2 data
-        data					: out std_logic_vector(7 downto 0);		-- tile data
-        addr					: out unsigned(10 downto 0);	-- tile address
-        we					: out std_logic);		-- write enable
+        data					: out std_logic_vector(17 downto 0);		-- tile data
+  
 end KBD_ENC;
 
 -- architecture
@@ -39,7 +38,9 @@ architecture behavioral of KBD_ENC is
   signal PS2state : state_type;					-- PS2 state
 
   signal ScanCode				: std_logic_vector(7 downto 0);	-- scan code
-  signal TileIndex				: std_logic_vector(7 downto 0);	-- tile index
+  signal keyPressed			: std_logic_vector(17 downto 0);	--Which_key_that_has_been_pressed
+                                                                   
+                                                                    
   
   type curmov_type is (FORWARD, BACKWARD, NEWLINE);		-- declare cursor movement types
   signal curMovement : curmov_type;				-- cursor movement
@@ -138,7 +139,7 @@ process(clk)
   -- *  PS2_State                      *
   -- *                                 *
   -- ***********************************
-
+ 
   process(clk)
   begin
     if rising_edge(clk) then
@@ -169,44 +170,55 @@ process(clk)
     end if;
   end process;
 
-	
+
+  
+  -- write state
+  -- every write cycle begins with writing the character tile index at the current
+  -- cursor position, then moving to the next cursor position and there write the
+  -- cursor tile index
+  process(clk)
+  begin
+    if rising_edge(clk) then
+      if rst='1' then
+        WRstate <= "000000000000000000"
+      else
+        case WRstate is
+          when STANDBY =>
+            if (PS2state = MAKE) then
+              WRstate <= WRCHAR;
+            else
+              WRstate <= STANDBY;
+            end if;
+          when WRCHAR =>
+            WRstate <= WRCUR;
+          when WRCUR =>
+            WRstate <= STANDBY;
+          when others =>
+            WRstate <= STANDBY;
+        end case;
+      end if;
+    end if;
+  end process;
+
+
+ -- Scan Code -> Tile Index mapping
+with ScanCode select
+    keyPressed <= "000000000000000001" when x"1C",	-- A
+    x"000000000000000010" when x"23",	-- D
+    x"000000000000000011" when x"1B",	-- S
+    x"000000000000000011" when x"1D",	-- W
+    x"000000000000000011" when x"29",	-- space
+    x"000000000000000000" when others;
+						 
+			
+-------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
+------ GENERERA GRAFUPPDATERING -------
+-------------------------------------------------------------------------------
+-------------------------------------------------------------------------------	
 	
 
-  -- Scan Code -> Tile Index mapping
-  with ScanCode select
-    TileIndex <= x"00" when x"29",	-- space
-    x"01" when x"1C",	-- A
-    x"02" when x"32",	-- B
-    x"03" when x"21",	-- C
-    x"04" when x"23",	-- D
-    x"05" when x"24",	-- E
-    x"06" when x"2B",	-- F
-    x"07" when x"34",	-- G
-    x"08" when x"33",	-- H
-    x"09" when x"43",	-- I
-    x"0A" when x"3B",	-- J
-    x"0B" when x"42",	-- K
-    x"0C" when x"4B",	-- L
-    x"0D" when x"3A",	-- M
-    x"0E" when x"31",	-- N
-    x"0F" when x"44",	-- O
-    x"10" when x"4D",	-- P
-    x"11" when x"15",	-- Q
-    x"12" when x"2D",	-- R
-    x"13" when x"1B",	-- S
-    x"14" when x"2C",	-- T
-    x"15" when x"3C",	-- U
-    x"16" when x"2A",	-- V
-    x"17" when x"1D",	-- W
-    x"18" when x"22",	-- X
-    x"19" when x"35",	-- Y
-    x"1A" when x"1A",	-- Z
-    x"1B" when x"54",  -- Å
-    x"1C" when x"52",  -- Ä
-    x"1D" when x"4C",  -- Ö
-    x"00" when others;
-						 
-						 
+			 
   -- set cursor movement based on scan code
   with ScanCode select
     curMovement <= NEWLINE when x"5A",	        -- enter scancode (5A), so move cursor to next line
@@ -214,11 +226,7 @@ process(clk)
                    FORWARD when others;	        -- for all other scancodes, move cursor forward
 
 
--------------------------------------------------------------------------------
--------------------------------------------------------------------------------
------- GENERERA GRAFUPPDATERING -------
--------------------------------------------------------------------------------
--------------------------------------------------------------------------------
+
   
   -- curposX
   -- update cursor X position based on current cursor position (curposX and curposY) and cursor
@@ -285,34 +293,6 @@ process(clk)
     end if;
   end process;
 
-
-  -- write state
-  -- every write cycle begins with writing the character tile index at the current
-  -- cursor position, then moving to the next cursor position and there write the
-  -- cursor tile index
-  process(clk)
-  begin
-    if rising_edge(clk) then
-      if rst='1' then
-        WRstate <= STANDBY;
-      else
-        case WRstate is
-          when STANDBY =>
-            if (PS2state = MAKE) then
-              WRstate <= WRCHAR;
-            else
-              WRstate <= STANDBY;
-            end if;
-          when WRCHAR =>
-            WRstate <= WRCUR;
-          when WRCUR =>
-            WRstate <= STANDBY;
-          when others =>
-            WRstate <= STANDBY;
-        end case;
-      end if;
-    end if;
-  end process;
 	
 
   -- we will be enabled ('1') for two consecutive clock cycles during WRCHAR and WRCUR states
@@ -326,7 +306,7 @@ process(clk)
 
   
   -- data output is set to be x"1F" (cursor tile index) during WRCUR state, otherwise set as scan code tile index
-  data <= x"1F" when (WRstate =  WRCUR) else TileIndex;
+  data <= x"1F" when (WRstate =  WRCUR) else keyPressed;
 
   
 end behavioral;
