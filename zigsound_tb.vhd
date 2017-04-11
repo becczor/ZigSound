e309abd698f38239165e4d436905b1341b62d493
Jennifer Lindgren <jennifer.lindgren93@gmail.com>
2017-04-11 17:14:53 +0200

    updated testbench with spaces and slower signals

--- zigsound_tb.vhd
+++ zigsound_tb.vhd
@@ -10,11 +10,11 @@ ARCHITECTURE behavior OF zigsound_tb IS
     --Component Declaration for the Unit Under Test (UUT)
     COMPONENT zigsound
         PORT(
-            clk                     : in std_logic;
-            btnd                    : in std_logic;
+            clk                         : in std_logic;
+            rst                         : in std_logic;
             -- VGA_MOTOR out
             vgaRed		        	: out std_logic_vector(2 downto 0);
-            vgaGreen	        	: out std_logic_vector(2 downto 0);
+            vgaGreen	        	        : out std_logic_vector(2 downto 0);
             vgaBlue		        	: out std_logic_vector(2 downto 1);
             Hsync		        	: out std_logic;
             Vsync		        	: out std_logic;
@@ -26,37 +26,109 @@ ARCHITECTURE behavior OF zigsound_tb IS
 
     --Inputs
     signal clk : std_logic := '0';
-    signal btnd : std_logic := '0';
-    signal PS2KeyboardCLK : std_logic := '0';  -- USB keyboard PS2 clock
-    signal PS2KeyboardData : std_logic := '0'; -- USB keyboard PS2 data
+    signal rst : std_logic := '0';
+    signal PS2KeyboardCLK : std_logic := '1';  -- USB keyboard PS2 clock
+    signal PS2KeyboardData : std_logic := '1'; -- USB keyboard PS2 data
+    signal tb_running : boolean := true;
     
-    -- Sabina says : NEJ NEJ JAG ORKAR INTE VI KOPPLAR IN DET FUCK IT.
+  
 
 
     --Clock period definitions
-    constant clk_period : time:= 1 us;
+    
 
 BEGIN
     -- Instantiate the Unit Under Test (UUT)
     uut: zigsound PORT MAP (
         clk => clk,
         rst => rst,
-        move_req => move_req,
-        curr_pos => curr_pos,
-        next_pos => next_pos,
-        sel_track => sel_track
+        --vgaRed => vgaRed,
+        --vgaGreen => vgaGreen,
+        --vgaBlue => vgaBlue,
+        --Hsync => Hsync,
+        --VSync => VSync,
+        PS2KeyboardCLK => PS2KeyboardCLK,
+        PS2KeyboardData => PS2KeyboardData
     );
 		
     -- Clock process definitions
-    clk_process :process
+    clk_gen : process
     begin
-        clk <= '0';
-        wait for clk_period/2;
-        clk <= '1';
-        wait for clk_period/2;
+        while tb_running loop
+            clk <= '0';
+            wait for 20 ns;
+            clk <= '1';
+            wait for 20 ns;
+        end loop;
+        wait;
     end process;
 
-    rst <= '1', '0' after 1.7 us;
-    move_req <= '1' after 2.0 us, '0' after 2.1 us;
+
+    ps2_clk_gen : process
+    begin
+        while tb_running loop
+            PS2KeyboardCLK <= '1';
+            wait for 1000 ns;
+            PS2KeyboardCLK <= '0';
+            wait for 1000 ns;
+        end loop;
+        wait;
+    end process;
+    
+    
+    ps2_code_gen : process
+    begin -- Send in two space presses
+        wait for 40000 ns;
+        PS2KeyboardData <= '0';
+        wait for 2000 ns;
+        PS2KeyboardData <= '1';
+        wait for 2000 ns;
+        PS2KeyboardData <= '0';
+        wait for 2000 ns;
+        PS2KeyboardData <= '0';
+        wait for 2000 ns;
+        PS2KeyboardData <= '1';
+        wait for 2000 ns;
+        PS2KeyboardData <= '0';
+        wait for 2000 ns;
+        PS2KeyboardData <= '1';
+        wait for 2000 ns;
+        PS2KeyboardData <= '0';
+        wait for 2000 ns;
+        PS2KeyboardData <= '0';
+        wait for 2000 ns;
+        PS2KeyboardData <= '0';
+        wait for 2000 ns;
+        PS2KeyboardData <= '1';  
+        
+        wait for 40000 ns;
+        PS2KeyboardData <= '0';
+        wait for 2000 ns;
+        PS2KeyboardData <= '1';
+        wait for 2000 ns;
+        PS2KeyboardData <= '0';
+        wait for 2000 ns;
+        PS2KeyboardData <= '0';
+        wait for 2000 ns;
+        PS2KeyboardData <= '1';
+        wait for 2000 ns;
+        PS2KeyboardData <= '0';
+        wait for 2000 ns;
+        PS2KeyboardData <= '1';
+        wait for 2000 ns;
+        PS2KeyboardData <= '0';
+        wait for 2000 ns;
+        PS2KeyboardData <= '0';
+        wait for 2000 ns;
+        PS2KeyboardData <= '0';
+        wait for 2000 ns;
+        PS2KeyboardData <= '1';  
+        wait;
+        
+        
+    end process;
+    
+       
 END;
+
 
